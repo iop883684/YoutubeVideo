1aaa3fc7f828052b9314bc676bde2d84e0c03e6e
Lac Tuan <lactuannn@gmail.com>
2017-12-25 17:46:24 +0700

    no message

---
+++ YoutubeVideo/Sources/Helpers/Define/ViewDefine.swift
@@ -0,0 +1,23 @@
+// MARK: - Storyboard
+struct Storyboard {
+    
+    struct Main {
+        static let name                         = "Main"
+
+    }
+    
+    struct User {
+        static let name                         = "User"
+
+    }
+    
+    struct Home {
+        static let name                         = "Home"
+    }
+    
+    struct Search {
+        static let name                         = "Search"
+    }
+    
+}
+
