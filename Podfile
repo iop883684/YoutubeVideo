83b2d3499c2355b6297d092d8bc141c67907ef4c
Lac Tuan <lactuannn@gmail.com>
2017-12-27 17:24:15 +0700

    aadsda

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'YoutubeVideo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for YoutubeVideo
+
+pod 'AlamofireObjectMapper', '~> 5.0'
+pod 'PKHUD', '~> 5.0'
+pod 'Kingfisher', '~> 4.0'
+
+end
---
+++ YoutubeVideo/Sources/Helpers/Extensions/TableView + Ext.swift
@@ -0,0 +1,31 @@
+//
+//  TableView + Ext.swift
+//  YoutubeVideo
+//
+//  Created by Lac Tuan on 12/25/17.
+//  Copyright © 2017 Lac Tuan. All rights reserved.
+//
+
+//MARK: - Quick Register nib
+
+import UIKit
+
+extension UITableView{
+    
+    func registerNib(_ nibClass: AnyClass,_ identifier: String){
+        let nib = UINib.init(nibName: String(describing: nibClass.self), bundle: nil)
+        
+        self.register(nib, forCellReuseIdentifier: identifier)
+    }
+}
+
+//MARK: - Quick Register nib
+
+extension UICollectionView {
+    
+    func registerNib(_ nibClass: AnyClass,_ identifier: String){
+        let nib = UINib.init(nibName: String(describing: nibClass.self), bundle: nil)
+        
+        self.register(nib, forCellWithReuseIdentifier: identifier)
+    }
+}
